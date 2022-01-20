4ac538763497d5a1b9aa47162fe739d3c26c19cf
Van den Berghe Jo <jo.vandenberghe@smartphoto.com>
2022-01-20 21:33:49 +0100

    Show coin with animation of myLocation

--- src/components/TopView/TopView.tsx
+++ src/components/TopView/TopView.tsx
@@ -3,65 +3,80 @@
 import createResizeObserver from "@solid-primitives/resize-observer";
 import { createGeolocationWatcher } from "@solid-primitives/geolocation";
 import {
-  createMemo, createSignal, For, Match, Switch,
+  Accessor,
+  createMemo, createSignal, For,
 } from "solid-js";
 
 import { state } from "../../store/store";
 import { Point } from "../../math/Point";
 import { Rect } from "../../math/Rect";
+
 import "../../math/features/fitInRect";
 import "../../math/features/transformPoint";
 
 import { SignalLogger } from "../SignalLogger/SignalLogger";
-import { createGeoSimulation } from "../../hooks/createGeoSimulation";
+import { createDragCoordinates } from "../../hooks/createDragCoordinates";
 import { createSpringValue, ISpringOptions } from "../../hooks/createSpring";
 import { MyWayPoint } from "../MyLocation/MyLocation";
 import { CoinWaypoint } from "../CoinWaypoint/CoinWaypoint";
 
 import styles from "./TopView.module.css";
 import { GeoLocationError } from "../GeoLocationError";
+import { throttleSignals } from "../../utils/signal-helpers";
 
 const springSettings: ISpringOptions = {
 };
 
 export const TopView = () => {
+  // Size of svg
   const [svgRect, setSvgRect] = createSignal(Rect.zero);
-  const viewRect = createMemo(() => svgRect().grow(-20));
-
-  const locationBounds = createMemo(() => Rect.fromPoints(state.waypoints.map((loc) => Point.create(loc.longitude, loc.latitude))));
-  const locationsToScreenTransform = createMemo(() => viewRect().fitRectTransform(locationBounds()));
-
-  const [location, error] = createGeolocationWatcher(true);
-
-  // Get screen location at drag
-  const [dragX, dragY] = createGeoSimulation();
-
   const refCallback = createResizeObserver({
     onResize: (size) => setSvgRect(Rect.create(0, 0, size.width, size.height)),
   });
 
-  // Animate screen position, not the GPS coordinate
-  const [myX, setMyX] = createSignal(0);
-  const [myY, setMyY] = createSignal(0);
+  const viewRect = createMemo(() => svgRect().grow(-20));
+
+  const locationBounds = createMemo(() => Rect
+    .fromPoints(state.waypoints.map((loc) => Point.create(loc.longitude, loc.latitude))));
+  const locationsToScreenTransform = createMemo(() => viewRect().fitRectTransform(locationBounds()));
+
+  const [location, error] = createGeolocationWatcher(true);
+
+  // Get Geo position from Drag
+  const [dragX, dragY] = createDragCoordinates();
+  // Throttle position to simulate geo coordinates intervals
+  const [throttledX, throttledY] = throttleSignals([dragX, dragY], 200) as [Accessor<number>, Accessor<number>];
+  // Convert to Geo locations
+  const simulatedLocation = createMemo(() => Point
+    .create(throttledX(), throttledY())
+    .transform(locationsToScreenTransform().inverse()));
+
+  // Location to use (simulated or real geo location)
+  const [myLon, setMyLon] = createSignal(0);
+  const [myLat, setMyLat] = createSignal(0);
 
   // Get screen location from GPS location
   createMemo(() => {
-    const myLocationOnScreen = Point
-      .create(location()?.longitude || 0, location()?.latitude || 0)
-      .transform(locationsToScreenTransform());
-
-    setMyX(myLocationOnScreen.left);
-    setMyY(myLocationOnScreen.top);
+    setMyLon(location()?.longitude || 0);
+    setMyLat(location()?.latitude || 0);
   });
 
   // When dragging, use it as target position
   createMemo(() => {
-    setMyX(dragX() || 0);
-    setMyY(dragY() || 0);
+    setMyLon(simulatedLocation().left || 0);
+    setMyLat(simulatedLocation().top || 0);
   });
 
-  const [smoothX] = createSpringValue(myX, springSettings);
-  const [smoothY] = createSpringValue(myY, springSettings);
+  // Convert location to screen coordinates
+  const myLocationOnScreen = createMemo(() => Point
+    .create(myLon() || 0, myLat() || 0)
+    .transform(locationsToScreenTransform()));
+  const myX = createMemo(() => myLocationOnScreen().left);
+  const myY = createMemo(() => myLocationOnScreen().top);
+
+  // Animate to new values
+  const [mySmoothX] = createSpringValue(myX, springSettings);
+  const [mySmoothY] = createSpringValue(myY, springSettings);
 
   return (
     <div
@@ -80,7 +95,7 @@ export const TopView = () => {
             return <CoinWaypoint x={point().left} y={point().top} />;
           }}</For>
 
-          <MyWayPoint gender="male" x={smoothX()} y={smoothY()} />
+          <MyWayPoint gender="male" x={mySmoothX()} y={mySmoothY()} />
         </svg>
       </GeoLocationError>
 
@@ -90,8 +105,11 @@ export const TopView = () => {
           locationBounds,
           locationsToScreenTransform,
           dragX,
+          simulatedLocation,
+          location: createMemo(() => location()?.longitude),
+          myLon,
           myX,
-          smoothX,
+          mySmoothX,
         }} />
       </div>
     </div>
--- src/hooks/createDragCoordinates.ts
+++ src/hooks/createDragCoordinates.ts
@@ -1,14 +1,13 @@
-import { Accessor, createMemo } from "solid-js";
-import { throttleSignals } from "../utils/signal-helpers";
+import { createMemo } from "solid-js";
 import { createMousePosition } from "./createMousePosition";
 
-export function createGeoSimulation() {
+export function createDragCoordinates() {
   const [mouseX, mouseY, buttons] = createMousePosition();
 
   // Only use mouse cooredinates while mouse down
+  const isDragging = createMemo(() => (buttons() === 1));
   const dragX = createMemo<number>((prev) => (buttons() === 1 ? mouseX() : prev || 0));
   const dragY = createMemo<number>((prev) => (buttons() === 1 ? mouseY() : prev || 0));
 
-  // Throttle position to simulate geo coordinates
-  return throttleSignals([dragX, dragY], 200) as [Accessor<number>, Accessor<number>];
+  return [dragX, dragY, isDragging];
 }
