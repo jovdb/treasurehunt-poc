14be09c314336618e76985c15bbe3c2d6fededd5
Van den Berghe Jo <jo.vandenberghe@smartphoto.com>
2022-01-23 01:21:01 +0100

    Play adio when captured

--- src/hooks/createCatchDetector.ts
+++ src/hooks/createCatchDetector.ts
@@ -1,11 +1,26 @@
-import { Accessor, createMemo } from "solid-js";
+/* eslint-disable no-plusplus */
+import { Accessor, batch, createMemo } from "solid-js";
 import { getDistanceFromLine } from "geolib";
 
 import { state, isCaptured, setCaptured } from "../store/store";
+import coinMp3Url from "../Audio/coin.mp3";
 
 function createLastValues<T>(accessor: Accessor<T>, length = 10) {
   return createMemo<T[]>((prev) => ([...prev, accessor()].slice(-length)), []);
 }
+
+/** Create 2 audio instances, so we can play before the previous is finished */
+function createAudio(mp3Url: string) {
+  const audio = [new Audio(mp3Url), new Audio(mp3Url)];
+  let count = 0;
+  return {
+    play() {
+      return audio[count++ % 2].play();
+    },
+  };
+}
+
+const coinAudio = createAudio(coinMp3Url);
 
 export function createCatcheDetector() {
   const location = createMemo(() => state.me?.location);
@@ -20,8 +35,15 @@ export function createCatcheDetector() {
       .filter((waypoint) => !isCaptured(waypoint.id))
       .filter((waypoint) => getDistanceFromLine(waypoint, prev, current) <= captureDistanceInMeter);
 
-    captured.forEach((waypoint) => {
-      setCaptured(waypoint.id);
+    batch(() => {
+      captured.forEach((waypoint) => {
+        setCaptured(waypoint.id);
+
+        if (waypoint.type === "coin") {
+          // eslint-disable-next-line @typescript-eslint/no-floating-promises
+          coinAudio.play();
+        }
+      });
     });
   });
 }
