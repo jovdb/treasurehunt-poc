c5742e1a7d37891e35d82ba13a8514b7d994f745
Van den Berghe Jo <jo.vandenberghe@smartphoto.com>
2022-01-26 23:11:21 +0100

    Allow moving position with keyboard

--- src/hooks/createLocationWatcher.ts
+++ src/hooks/createLocationWatcher.ts
@@ -1,11 +1,15 @@
+import { findNearest } from "geolib";
+
 import { createGeolocationWatcher } from "@solid-primitives/geolocation";
-import { Accessor, createMemo, createSignal } from "solid-js";
+import {
+  Accessor, createMemo, createSignal, onCleanup, onMount,
+} from "solid-js";
 
 import { Point } from "../math/Point";
 import { Transform } from "../math/Transform";
 import { ILocation } from "../types/WayPoint";
 
-import { setMyLocation } from "../store/store";
+import { setMyLocation, state } from "../store/store";
 import { throttleSignals } from "../utils/signal-helpers";
 import { createDragCoordinates } from "./createDragCoordinates";
 
@@ -13,6 +17,84 @@ export function createLocationWatcher(
   locationsToScreenTransform: Accessor<Transform>,
 ) {
   const [location, locationError] = createGeolocationWatcher(true);
+
+  // Location to use (simulated or real geo location)
+  const [myLon, setMyLon] = createSignal(0);
+  const [myLat, setMyLat] = createSignal(0);
+
+  function getClosestWaypoint() {
+    const closestLocation = findNearest({
+      latitude: myLat(),
+      longitude: myLon(),
+    }, state.waypoints as unknown as ILocation[]) as ILocation;
+
+    const index = state.waypoints.findIndex((waypoint) => waypoint.longitude === closestLocation.longitude && waypoint.latitude === closestLocation.latitude);
+    return [state.waypoints[index], index] as const;
+  }
+
+  // Change location with keypresses (dragging won't help if we move the map)
+  function onKeyDown(e: KeyboardEvent) {
+    switch (e.key) {
+      case "Home": {
+        const { longitude, latitude } = state.waypoints[0];
+        setMyLon(longitude);
+        setMyLat(latitude);
+        break;
+      }
+
+      case "End": {
+        const { longitude, latitude } = state.waypoints[state.waypoints.length - 1];
+        setMyLon(longitude);
+        setMyLat(latitude);
+        break;
+      }
+
+      case "PageUp": {
+        const [, index] = getClosestWaypoint();
+        const nextIndex = Math.max(0, Math.min(state.waypoints.length - 1, index + 1));
+        const nextWaypoint = state.waypoints[nextIndex];
+        setMyLon(nextWaypoint.longitude);
+        setMyLat(nextWaypoint.latitude);
+        break;
+      }
+
+      case "PageDown": {
+        const [, index] = getClosestWaypoint();
+        const nextIndex = Math.max(0, Math.min(state.waypoints.length - 1, index - 1));
+        const nextWaypoint = state.waypoints[nextIndex];
+        setMyLon(nextWaypoint.longitude);
+        setMyLat(nextWaypoint.latitude);
+        break;
+      }
+
+      case "ArrowLeft":
+        setMyLon((prev) => prev - 0.00005);
+        break;
+
+      case "ArrowRight":
+        setMyLon((prev) => prev + 0.00005);
+        break;
+
+      case "ArrowUp":
+        setMyLat((prev) => prev + 0.00003);
+        break;
+
+      case "ArrowDown":
+        setMyLat((prev) => prev - 0.00003);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  onMount(() => {
+    document.addEventListener("keydown", onKeyDown);
+  });
+
+  onCleanup(() => {
+    document.addEventListener("keydown", onKeyDown);
+  });
 
   // Get Geo position from Drag
   const [dragX, dragY] = createDragCoordinates();
@@ -22,10 +104,6 @@ export function createLocationWatcher(
   const simulatedLocation = createMemo(() => Point
     .create(throttledX(), throttledY())
     .transform(locationsToScreenTransform().inverse()));
-
-  // Location to use (simulated or real geo location)
-  const [myLon, setMyLon] = createSignal(0);
-  const [myLat, setMyLat] = createSignal(0);
 
   // Get screen location from GPS location
   createMemo(() => {
